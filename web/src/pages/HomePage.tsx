4b3407dc78900eb2631cf224498d020e283e8e02
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2026-08-29 22:36:22 -0300

    chore: modifica url do swagger no frontend

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -25,7 +25,7 @@ export function HomePage() {
             </Typography>
             <Button
               variant="contained"
-              href="http://localhost:8080/swagger-ui.html"
+              href="http://localhost:8080/swagger"
             >
               Abrir documentação da API
             </Button>
