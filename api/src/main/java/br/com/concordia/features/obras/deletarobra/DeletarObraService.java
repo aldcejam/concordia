6e353b0721592d08a84f404d302deb640b7e3891
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2026-09-06 19:48:38 +0000

    feat: cria endpoints de DeletarObra

---
+++ api/src/main/java/br/com/concordia/features/obras/deletarobra/DeletarObraService.java
@@ -0,0 +1,26 @@
+package br.com.concordia.features.obras.deletarobra;
+
+import br.com.concordia.common.application.exceptions.RecursoNaoEncontradoException;
+import br.com.concordia.common.infrastructure.repositories.ObraRepository;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class DeletarObraService {
+    private final ObraRepository repository;
+
+    public DeletarObraService(ObraRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public void deletarObra(UUID id) {
+        var obra = repository
+                .findById(id)
+                .orElseThrow(
+                        () -> new RecursoNaoEncontradoException("Obra com ID %s não foi encontrada".formatted(id)));
+
+        repository.delete(obra);
+    }
+}
